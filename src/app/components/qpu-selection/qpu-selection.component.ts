919cfb66185252203ad649491ec95411fd256ca5
salmma <marie.salm@iaas.uni-stuttgart.de>
2022-05-11 18:36:45 +0200

    tidy up

--- src/app/components/qpu-selection/qpu-selection.component.ts
+++ src/app/components/qpu-selection/qpu-selection.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-qpu-selection',
   templateUrl: './qpu-selection.component.html',
-  styleUrls: ['./qpu-selection.component.scss']
+  styleUrls: ['./qpu-selection.component.scss'],
 })
 export class QpuSelectionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
