866bf153671026525b07ff7ee7b297845e610437
salmma <marie.salm@iaas.uni-stuttgart.de>
2022-05-24 18:51:25 +0200

    fix module errors

--- src/app/components/implementation-view/implementation-view.module.ts
+++ src/app/components/implementation-view/implementation-view.module.ts
@@ -6,7 +6,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,49 +14,61 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatSortModule } from '@angular/material/sort';
-import { AddImplementationDialogComponent } from '../algorithms-implementations-list/dialogs/add-implementation-dialog/add-implementation-dialog.component';
-import { CreateSdkDialogComponent } from './implementation-selection-criteria/dialogs/create-sdk-dialog/create-sdk-dialog.component';
-import { QpuSelectionComponent } from './qpu-selection/qpu-selection.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ImplementationSelectionCriteriaComponent } from './implementation-selection-criteria/implementation-selection-criteria.component';
 import { ImplementationViewComponent } from './implementation-view.component';
+import { CreateSdkDialogComponent } from './implementation-selection-criteria/dialogs/create-sdk-dialog/create-sdk-dialog.component';
+import { QpuSelectionComponent } from './qpu-selection/qpu-selection.component';
+import { QpuSelectionExecutionDialogComponent } from './qpu-selection/dialogs/qpu-selection-execution-dialog/qpu-selection-execution-dialog.component';
 // eslint-disable-next-line max-len
 import { QpuSelectionSensitivityAnalysisDialogComponent } from './qpu-selection/dialogs/qpu-selection-sensitivity-analysis-dialog/qpu-selection-sensitivity-analysis-dialog.component';
 // eslint-disable-next-line max-len
-import { QpuSelectionLearnedWeightsDialogComponent } from './qpu-selection/dialogs/qpu-selection-learned-weights-dialog/qpu-selection-learned-weights-dialog.component';
-// eslint-disable-next-line max-len
 import { QpuSelectionPrioritizationDialogComponent } from './qpu-selection/dialogs/qpu-selection-prioritization-dialog/qpu-selection-prioritization-dialog.component';
-import { QpuSelectionExecutionDialogComponent } from './qpu-selection/dialogs/qpu-selection-execution-dialog/qpu-selection-execution-dialog.component';
+// eslint-disable-next-line max-len
+import { QpuSelectionLearnedWeightsDialogComponent } from './qpu-selection/dialogs/qpu-selection-learned-weights-dialog/qpu-selection-learned-weights-dialog.component';
+import { QpuSelectionDialogComponent } from './qpu-selection/dialogs/qpu-selection-dialog/qpu-selection-dialog.component';
 
 @NgModule({
   declarations: [
     ImplementationViewComponent,
     ImplementationSelectionCriteriaComponent,
     QpuSelectionComponent,
-    AddImplementationDialogComponent,
     CreateSdkDialogComponent,
+    QpuSelectionDialogComponent,
     QpuSelectionSensitivityAnalysisDialogComponent,
     QpuSelectionLearnedWeightsDialogComponent,
     QpuSelectionPrioritizationDialogComponent,
     QpuSelectionExecutionDialogComponent,
   ],
   imports: [
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatDialogModule,
-    MatSelectModule,
-    FormsModule,
     CommonModule,
-    MatIconModule,
-    MatCardModule,
-    MatProgressSpinnerModule,
-    MatTabsModule,
-    MatInputModule,
+    FormsModule,
     MatButtonModule,
-    MatTableModule,
-    MatStepperModule,
+    MatCardModule,
     MatCheckboxModule,
+    MatDialogModule,
     MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
     MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+  ],
+  exports: [
+    ImplementationViewComponent,
+    ImplementationSelectionCriteriaComponent,
+    QpuSelectionComponent,
+    CreateSdkDialogComponent,
+    QpuSelectionDialogComponent,
+    QpuSelectionSensitivityAnalysisDialogComponent,
+    QpuSelectionLearnedWeightsDialogComponent,
+    QpuSelectionPrioritizationDialogComponent,
+    QpuSelectionExecutionDialogComponent,
   ],
 })
 export class ImplementationViewModule {}
