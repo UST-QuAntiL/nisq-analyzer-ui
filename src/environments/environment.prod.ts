4c3f07bedf2b782dd11576c9cdd02e5e7ca634a7
salmma <marie.salm@iaas.uni-stuttgart.de>
2022-05-24 20:37:04 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
-import { environment as e } from './environment';
-
 export const environment = {
-  ...e,
-  production: true,
+    production: true,
+    nisqAnalyzer: window['env']['nisqAnalyzer'] || true,
+    NISQ_API_URL:
+        window['env']['NISQ_ANALYZER_HOST_NAME'] &&
+        window['env']['NISQ_ANALYZER_PORT']
+            ? `http://${window['env']['NISQ_ANALYZER_HOST_NAME']}:${window['env']['NISQ_ANALYZER_PORT']}/nisq-analyzer`
+            : 'http://localhost:5010/nisq-analyzer',
 };
